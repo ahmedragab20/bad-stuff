a05062d3e6034e7da6b237d197ded1bc42ec97af
ahmedragab <ahmedragab20901@gmail.com>
2024-01-21 13:14:37 +0200

    🦉.. Recursion implementation

--- script.js
+++ script.js
@@ -1,49 +1,78 @@
-addEventListener("DOMContentLoaded", (e) => {
-  const d1 = document.querySelector("#d1");
-  const d2 = document.querySelector("#d2");
-  const drop_zone = document.querySelector("#drop_zone");
+let questions = [
+  {
+    id: 1,
+    isCorrect: null,
+  },
+  {
+    id: 2,
+    isCorrect: null,
+  },
+  {
+    id: 3,
+    isCorrect: null,
+  },
+  {
+    id: 4,
+    isCorrect: null,
+  },
+];
+let wrongAnsweredQuestions = [];
+let count = 0;
+const clone = (value) => JSON.parse(JSON.stringify(value));
 
-  let ondragEl = null;
+// null -> true ✅
+// null -> false -> null ✅
 
-  d1.addEventListener("dragstart", (e) => {
-    console.log("Dargstart 🔥");
-    console.log(e);
-    ondragEl = e.target;
-  });
+function loop() {
+  console.log(
+    `%c${count + 1}`,
+    "font-weight: bold; font-size: 18px; color: cadetblue;"
+  );
+  let loopArray = wrongAnsweredQuestions.length
+    ? clone(wrongAnsweredQuestions)
+    : clone(questions);
 
-  d1.addEventListener("dragend", (e) => {
-    console.log("Dargend ✅");
-    ondragEl = null;
-    console.log(ondragEl);
-  });
-
-  drop_zone.addEventListener("dragenter", (event) => {
-    console.log("Dargenter 🛬");
-
-    event.currentTarget.style.scale = "1.05";
-
-    event.preventDefault();
-  });
-
-  drop_zone.addEventListener("dragleave", (event) => {
-    event.preventDefault();
-    console.log("Dargleave 👋🏻");
-
-    event.currentTarget.style.removeProperty("scale");
-  });
-
-  drop_zone.addEventListener("dragover", (event) => {
-    event.preventDefault();
-    console.log("dragover 🦉");
-  });
-
-  drop_zone.addEventListener("drop", (event) => {
-    event.preventDefault();
-    console.log("Dropped 🫳🏻");
-    console.log(event);
-
-    if (ondragEl) {
-      event.target.appendChild(ondragEl);
+  loopArray.forEach((element) => {
+    if ([2, 3].includes(element.id) && !count /* 0 */) {
+      element.isCorrect = false;
+      return;
     }
+    element.isCorrect = true;
   });
-});
+
+  wrongAnsweredQuestions = loopArray
+    .filter((el) => !el.isCorrect)
+    .map((el) => {
+      return {
+        ...el,
+        isCorrect: null,
+      };
+    });
+  console.log("loop final result: ", questions);
+  console.log(wrongAnsweredQuestions);
+  if (!count) {
+    document.body.style.background = "salmon";
+  } else {
+    document.body.style.background = "cornflowerblue";
+  }
+
+  count++;
+  document.body.innerHTML = /*html*/`
+    <div>
+      <h1>🦉...${count}</h1>
+    </div>
+  `;
+  if (wrongAnsweredQuestions.length) {
+    loop();
+  } else {
+    questions = questions.map((el) => {
+      return {
+        ...el,
+        isCorrect: true,
+      };
+    });
+  }
+}
+
+loop();
+console.log("final result: ", questions);
